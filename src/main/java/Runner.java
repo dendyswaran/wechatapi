3bbbbb23bf66ce3cedac180d65fb90724ec51b87
dendyswaran <dendy.swarandanu@fairtech.com.sg>
2016-10-30 18:54:26 +0700

    Initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,25 @@
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.ext.web.Router;
+
+/**
+ * Created by dendy on 10/30/16.
+ */
+public class Runner extends AbstractVerticle {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Runner.class);
+
+    @Override
+    public void start() throws Exception {
+        super.start();
+
+        final Router router = Router.router(vertx);
+        router.route().handler(ctx -> {
+           ctx.response().end("Hello World!");
+        });
+
+        vertx.createHttpServer().requestHandler(router::accept).listen(10000);
+        LOG.info("Server started on port 1000");
+    }
+}
